d75cce0974cd011d7fd17a0a52f805720548a322
mirasildev <asilfr.dev@gmail.com>
2023-01-26 16:07:14 +0500

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	pb "github.com/mirasildev/medium_notification_service/genproto/notification_service"
+
+	"github.com/mirasildev/medium_notification_service/config"
+	"github.com/mirasildev/medium_notification_service/service"
+)
+
+func main() {
+	cfg := config.Load(".")
+
+	notificationService := service.NewNotificaionService(&cfg)
+
+	lis, err := net.Listen("tcp", cfg.GrpcPort)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+
+	pb.RegisterNotificationServiceServer(s, notificationService)
+
+	log.Println("Grpc server started in port ", cfg.GrpcPort)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Error while listening: %v", err)
+	}
+}
